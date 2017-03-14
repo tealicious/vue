13875eeb304e29a0b0b2fd87caeb9d7d458604f6
tealicious <carterdanielr@gmail.com>
2017-03-14 16:04:56 -0400

    going over old lessons

---
+++ js/boxes.js
@@ -0,0 +1,37 @@
+new Vue({
+  el: '#app',
+  data: {
+    colorToggle  : false,
+    color        : 'green',
+    inlineColor  : 'white',
+    width        : 22
+  },
+  computed: {
+    divClassRed: function() {
+      return {
+        red: this.colorToggle
+      };
+    },
+    divClassBlue: function() {
+      return {
+        blue: this.colorToggle
+      };
+    },
+    divClassGreen: function() {
+      return {
+        green: this.colorToggle
+      };
+    },
+    myColorStyle: function() {
+      return {
+        backgroundColor: this.inlineColor
+      };
+    },
+    myWidthStyle: function() {
+      return {
+        backgroundColor: this.inlineColor,
+        width: this.width + 'vmin'
+      };
+    }
+  }
+});
