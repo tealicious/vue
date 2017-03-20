f4aed984124fbed7328aabf9ab6312d1c64f595e
tealicious <carterdanielr@gmail.com>
2017-03-20 16:31:16 -0400

    .

---
+++ js/lists.js
@@ -0,0 +1,11 @@
+new Vue({
+	el: '#app',
+	data: {
+		ingredients:['meat','fruit', 'cookies'],
+    persons: [
+      {name: 'Max', age: 27, color: 'red'},
+      {name: 'Anna', age: 'unknown', color: 'blue'}
+    ],
+    foods: ['meat','fruit', 'cookies']
+	}
+})
