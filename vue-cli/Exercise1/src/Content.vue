a021c0e5d04ea4c961223ae18266d86541372da4
Tealicious <carterdanielr@gmail.com>
2017-04-30 21:24:21 -0400

    .

---
+++ vue-cli/Exercise1/src/Content.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <server-count></server-count>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <server-status></server-status>
+      </div>
+  </div>
+</template>
+
+<script>
+import ServerStatus from './Servers.vue'
+import ServerCount from './ServerCount.vue'
+export default {
+  components: {
+    'server-status': ServerStatus,
+    'server-count': ServerCount
+  }
+}
+</script>
