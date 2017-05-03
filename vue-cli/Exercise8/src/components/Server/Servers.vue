46b1d8c3f5c7a7aac844e9ff975b0ad145d6beed
tealicious <carterdanielr@gmail.com>
2017-05-03 16:48:27 -0400

    .

--- vue-cli/Exercise8/src/components/Server/Servers.vue
+++ vue-cli/Exercise8/src/components/Server/Servers.vue
@@ -4,15 +4,24 @@
             <li
                     class="list-group-item"
                     v-for="server in servers">
-                Server #{{ id }}
+                <p class='number'>
+                  Server #{{ server.id }}
+                </p>
+                <p class='status'>
+                  {{ server.status}}
+                </p>
             </li>
         </ul>
     </div>
 </template>
 
 <script>
-  // assignment: loop through the servers, make them clickable and the click event to load the server data with the ServerDetails component
+  // assignment: loop through the servers, make them clickable and make the click event load the server data into the ServerDetails component
   // Once the server is in the serverDetails component, place a button that will change the status in both components back to 'normal'
+
+
+  import { eventBus } from '../../main'; //move up 2 directories to find main.js
+
   export default {
     data: function() {
       return {
@@ -27,6 +36,18 @@
   }
 </script>
 
+
+
 <style>
+.list-group-item {
+  display:flex;
+  flex-flow:row wrap;
+  justify-content:space-between;
+  align-items:center;
+  cursor:pointer;
+}
+p {
+  margin-bottom:0;
+}
 
 </style>
--- vue-cli/Exercise8/src/main.js
+++ vue-cli/Exercise8/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//create eventBus to share components between sibling components
+export const eventBus = new Vue();
+
 new Vue({
   el: '#app',
   render: h => h(App)
