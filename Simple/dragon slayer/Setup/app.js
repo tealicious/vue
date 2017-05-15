ea9434837d6877b8ebb523ebeb7d746c754c345f
tealicious <carterdanielr@gmail.com>
2017-05-15 10:04:33 -0400

    .

--- Simple/dragon slayer/Setup/app.js
+++ Simple/dragon slayer/Setup/app.js
@@ -3,65 +3,98 @@ var vm = new Vue({
   data: {
     playerHealth  : 100,
     dragonHealth  : 100,
-    gameIsRunning : false
+    gameIsRunning : false,
+    turns: []
   },
   methods: {
     startGame: function() {
+      this.turns = [];
       this.gameIsRunning = true;
-      this.playerHealth = 100;
-      this.dragonHealth = 100;
+      var newThis = this;
+      setTimeout(function(){
+        newThis.playerHealth = 100;
+        newThis.dragonHealth = 100;
+      }, 500);
     },
     attack: function() {
       var damage = this.calculateDamage(3, 10);
       this.dragonHealth -= damage;
-
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
       this.checkWin();
-
       var newThis = this;
       setTimeout(function(){
-        newThis.playerHealth -= Math.floor(damage/1.25);
+        var playerDamage = Math.floor(damage/1.25);
+        newThis.playerHealth -= playerDamage;
+        newThis.turns.unshift({
+          isPlayer: false,
+          text: 'Dragon hits player for ' + playerDamage
+        });
       }, 150);
-
-
     },
     specialAttack: function() {
       var damage = this.calculateDamage(5, 15);
       this.dragonHealth -= damage;
-
-      if(this.dragonHealth <= 0){
-          alert('You won!');
-          this.gameIsRunning = false;
-          return;
-      }
-
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
+      this.checkWin();
       var newThis = this;
       setTimeout(function(){
-        newThis.playerHealth -= Math.floor(damage/1.11);
+        var playerDamage = Math.floor(damage/1.25);
+        newThis.playerHealth -= playerDamage;
+        newThis.turns.unshift({
+          isPlayer: false,
+          text: 'Dragon hits player for ' + playerDamage
+        });
       }, 150);
 
-      if (this.playerHealth <= 0){
-          alert('you lost! that dragon just fucking ate you bro.');
-          this.gameIsRunning = false;
-          return;
-      }
     },
     heal: function() {
-
+      var healing = this.calculateHealing(5, 15);
+      var damage = this.calculateDamage(3, 10);
+      this.playerHealth += healing;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for ' + healing
+      });
+      var newThis = this;
+      setTimeout(function(){
+        newThis.playerHealth -= damage;
+        newThis.turns.unshift({
+          isPlayer: false,
+          text: 'Dragon hits player for ' + damage
+        });
+      }, 500);
     },
     giveUp: function() {
-
+      if(confirm('Give Up?? Are you sure??')){
+        if(confirm('Play again?')){
+          this.startGame();
+        }else {
+          this.gameIsRunning = false;
+        }
+      }
+    },
+    calculateHealing: function(min, max){
+      return Math.max(Math.floor((Math.random() * max) + 1, min));
     },
     calculateDamage: function(min, max){
       return Math.max(Math.floor((Math.random() * max) + 1, min));
     },
     checkWin: function() {
       if(this.dragonHealth <= 0){
+          this.dragonHealth = 0;
           if(confirm('You won! New Game?')){
             this.startGame();
           } else {
             this.gameIsRunning = false;
           } return true;
         } else if (this.playerHealth <= 0) {
+            this.playerHealth = 0;
             if(confirm('you lost! that dragon just fucking ate you bro. play again?')) {
               this.startGame();
             } else {
