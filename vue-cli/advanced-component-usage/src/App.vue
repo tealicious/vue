1934104255df4a172e82bb09677365e55b1179d7
tealicious <carterdanielr@gmail.com>
2017-09-07 17:13:39 -0400

    'o/'

---
+++ vue-cli/advanced-component-usage/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <quote></quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    export default {
+        data: function() {
+            return {
+                airquote : "A wonderful quote"
+            }
+        },
+        components: {
+            quote : Quote
+        }
+    }
+</script>
+
+<style>
+</style>
