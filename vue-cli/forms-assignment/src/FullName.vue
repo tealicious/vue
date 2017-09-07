f7786edf101e35db3529ff1f90bd2a5062fd090f
tealicious <carterdanielr@gmail.com>
2017-09-07 15:18:48 -0400

    forms assignment

---
+++ vue-cli/forms-assignment/src/FullName.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class='form-group'>
+     <label for="first-name">First Name</label>
+     <input required id="first-name" class="form-control" type='text'
+     :value="firstName" @input="nameChanged(true, $event)"/>
+     </div>
+     <div class='form-group'>
+       <label for="last-name">Last Name</label>
+       <input required id="last-name" class="form-control" type="text"
+       :value="lastName" @input="nameChanged(false, $event)"/>
+     </div>
+  </div>
+</template>
+<script>
+  export default {
+    props:['value'],
+    methods: {
+      nameChanged(isFirst, event) {
+        let name = '';
+        if ( isFirst ) {
+          name = event.target.value + ' ' + this.lastName;
+        } else {
+          name = this.firstName + ' ' + event.target.value;
+        }
+        this.value = name;
+        this.$emit('input', this.value)
+      }
+    },
+    computed: {
+      firstName() {
+        return this.value.split(" ")[0];
+      },
+      lastName() {
+        return this.value.split(" ")[1];
+      }
+    }
+  }
+</script>
