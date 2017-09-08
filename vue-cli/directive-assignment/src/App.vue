bc1800ca581029c4198d9b9b26b10fd454108970
tealicious <carterdanielr@gmail.com>
2017-09-08 12:05:45 -0400

    my directive assignment

--- vue-cli/directive-assignment/src/App.vue
+++ vue-cli/directive-assignment/src/App.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
+                <h1 v-click-dis:grow.fade='{fontSize: "55px"}'>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
 
@@ -12,7 +12,20 @@
 </template>
 
 <script>
-    export default {}
+    export default {
+      directives: {
+        'click-dis': {
+          bind(el, binding, vnode) {
+            function doTheThing(){
+                if(binding.arg = 'grow'){
+                    el.style.fontSize = binding.value.fontSize;
+                }
+            }
+            el.addEventListener('click', doTheThing);
+          }
+        }
+      }
+    }
 </script>
 
 <style>
