ac22f792e88f3fdc0cc6d931a79aae4744d937bf
tealicious <carterdanielr@gmail.com>
2017-09-12 12:18:33 -0400

    HTTP fin

--- vue-cli/Http/src/App.vue
+++ vue-cli/Http/src/App.vue
@@ -3,6 +3,12 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <select class="form-control" v-model="node">
+                  <option>data</option>
+                  <option>alternative</option>
+                </select>
+                <br />
+                <br />
                 <form v-on:submit.prevent="submit">
                     <div class='form-group'>
                       <label>Username</label>
@@ -31,32 +37,6 @@
                         </li>
                     </transition-group>
                 </ul>
-                <hr />
-                <br />
-                <div class='form-group'>
-                  <label>Band</label>
-                  <input class='form-control' type='text' required="true"
-                  v-model="band.name"/>
-                </div>
-                <div class='form-group'>
-                  <label>Album</label>
-                  <input class='form-control' type='text' required="true"
-                  v-model="band.album"/>
-                </div>
-                <button class='btn btn-info' @click="getAlbumList">Search Music</button>
-                <br />
-                <br />
-                <ul class="list-group">
-                    <transition-group name="fade" mode="out-in">
-                        <li class='list-group-item'
-                            style="cursor:pointer"
-                            v-for="(album, index) in musicSearch"
-                            :key="index"
-                            @click="remove">
-                            <img :src="album.thumb" />
-                        </li>
-                    </transition-group>
-                </ul>
             </div>
         </div>
     </div>
@@ -67,74 +47,70 @@
         data() {
             return {
                 user: {
-                    name:'',
-                    email:''
+                    name:'whoDat',
+                    email:'boy@1000yearsRickandMorty.com'
                 },
                 users: [],
-                musicSearch: [],
                 requestUrl: "https://vuexhr.firebaseio.com/data.json",
-                requestMusic: "http://api.discogs.com/database/search?",
-                bandParam: "artist=",
-                albumParam: "&release_title=",
-                Oauth: "&key=COmwGBAqWkMRnFFmdiON&secret=vffgxgmpqRGbjVkRyJihMZBaOluuoHXY",
-                band: {
-                    name: 'nirvana',
-                    album: 'nevermind'
-                }
+                resource: {},
+                node:'data'
             }
         },
         methods: {
-             getAlbumList() {
-                const requestString = this.bandParam + this.band.name + this.albumParam + this.band.album;
-                const callParams = this.requestMusic + requestString + this.Oauth;
-                this.$http.jsonp(callParams)
-                    .then(response => {
-                        return response.json();
-                    }).then( fetchedData => {
-                        const searchReturn = fetchedData.data.results;
-                        const musicSearchArray = [];
-                        for (let key in searchReturn) {
-                                // use the unique keynames to access their user objects
-                                musicSearchArray.push(searchReturn[key]);
-                        }
-                         this.musicSearch = musicSearchArray;
-                    }, error => {
-                        console.log(error);
-                    });
-            },
             remove(index) {
                 this.users.splice(index, 1);
             },
             submit() {
-                console.log(this.user);
+                this.resource.save({node: this.node}, this.user);
+                //this.resource.saveAlt({node: this.node}, this.user);
                 // Vue XML http request or AJAX replacement
                 // vue-resource provides the $http object to work with
                 // args
                 // 1. URL to post to ('data.json must be added for this example in order for firebase to work')
                 // 2. object to send
-                this.$http.post(this.requestUrl, this.user)
-                .then(response => { //once we get response back, do something with it
-                    console.log(response.body.name);
-                }, error => {
-                    console.log(error);
-                });
+                // this.$http.post('data.json', this.user)
+                // .then(response => { //once we get response back, do something with it
+                //     console.log("response body name: " + response.body.name);
+                // }, error => {
+                //     console.log(error);
+                // });
             },
             fetchData() {
-                this.$http.get(this.requestUrl)
-                .then(response => {
-                    console.log(response.json());
-                    return response.json(); // the response.json object must be chained into another .then() method to have its contents extraced, otherwise we just receive a promise object
-                }).then( fetchedData => {
-                    const resultArray = [];
-                    for (let key in fetchedData) {
-                            // use the unique keynames to access their user objects
-                            resultArray.push(fetchedData[key]);
-                    }
-                    this.users = resultArray;
-                }, error => {
-                    console.log(error);
-                });
+                this.resource.getData({node: this.node})
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then( fetchedData => {
+                        const resultArray = [];
+                        for (let key in fetchedData) {
+                                // use the unique keynames to access their user objects
+                                resultArray.push(fetchedData[key]);
+                        }
+                        this.users = resultArray;
+                    }, error => {
+                        console.log(error);
+                    });
+                // this.$http.get('data.json')
+                // .then(response => {
+                //     return response.json(); // the response.json object must be chained into another .then() method to have its contents extraced, otherwise we just receive a promise object
+                // }).then( fetchedData => {
+                //     const resultArray = [];
+                //     for (let key in fetchedData) {
+                //             // use the unique keynames to access their user objects
+                //             resultArray.push(fetchedData[key]);
+                //     }
+                //     this.users = resultArray;
+                // }, error => {
+                //     console.log(error);
+                // });
             }
+        },
+        created() {
+            const customActions = {
+                saveAlt: {method: 'POST'},
+                getData: {method: 'GET'}
+            };
+            this.resource = this.$resource('{node}.json', {}, customActions);
         }
     }
 </script>
--- vue-cli/Http/src/main.js
+++ vue-cli/Http/src/main.js
@@ -3,8 +3,22 @@ import VueResource from 'vue-resource'
 import App from './App.vue'
 
 Vue.use(VueResource)
+
 // Can store request URL globally and provide an empty string OR append to the end on the VUE ajax calls where you would normally pass the full url
-//Vue.http.options.root = "https://vuexhr.firebaseio.com/data.json"
+Vue.http.options.root = "https://vuexhr.firebaseio.com/"
+
+//intercept and manipulate XHR's
+// Vue.http.interceptors.push((request, next) => {
+//   console.log(request);
+//   if(request.method == 'POST'){
+//     //change post method to put (overwrite data there)
+//     request.method = 'PUT';
+//   }
+//   next(response => {
+//     //create an object to console log the response body to (during GET request)
+//     response.json = () => { return {messages: response.body} }
+//   });
+// });
 
 new Vue({
   el: '#app',
