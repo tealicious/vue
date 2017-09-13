8b689840926446c8339cba863890035df74ca642
tealicious <carterdanielr@gmail.com>
2017-09-13 16:31:29 -0400

    routes

--- vue-cli/routing/src/routes.js
+++ vue-cli/routing/src/routes.js
@@ -7,10 +7,10 @@ import UserStart from './components/user/UserStart.vue';
 export const routes =  [
   { path: '', component: Home},
   //{ path: '/user/:id', component: User, props: true }, // '/:{{key}}' expects user/some-sub-url-path
-  { path: '/user', component: User, props: true, children: [
-    { path: '', component: UserStart, props: true }, // a new nav template
-    { path: ':id', component: UserDetail , props: true}, // a pariable id (i.e. userName)
-    { path: ':id/edit', component: UserEdit, name: 'userEdit' } // some alt view paried to that id
-    // nested routes need to have a new component created somewhere (Use.vue)
-  ] }
+  { path: '/user', component: User, children: [ // I THINK the children object automatically tells the router that <router-view> will start rendering the components designated to it
+    { path: '', component: UserStart }, // @ no string render the UserStart.vue component
+    { path: ':id', component: UserDetail ,}, // @id (we use names here) render the url + '/id' via the userDetail component
+    { path: ':id/edit', component: UserEdit, name: 'userEdit' } // I think the name key bings the links in the vue to this route ? not sure
+  ] },
+  { path: '/redirect-me', redirect: '/user'}
 ];
