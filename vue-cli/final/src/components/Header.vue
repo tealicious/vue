2919fb7bbb6f85d40ee068687c172c35f1e89ea4
tealicious <carterdanielr@gmail.com>
2017-09-18 12:52:56 -0400

    navbar and route links added - final project

--- vue-cli/final/src/components/Header.vue
+++ vue-cli/final/src/components/Header.vue
@@ -1,3 +1,24 @@
 <template>
-<h1>The Header Component</h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #e3f2fd;">
+  <router-link class="navbar-brand" to="/">Stock Trader</router-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <router-link class="nav-item" to="/portfolio" activeClass="active" tag="li"><a class="nav-link">Portfolio</a></router-link>
+      <router-link class="nav-item" to="/stocks" activeClass="active" tag="li"><a class="nav-link">Stocks</a></router-link>
+    </ul>
+    <div class="dropdown show">
+      <a class="btn btn-warning dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Save & Load
+    </a>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+        <a class="dropdown-item" href="#">Save</a>
+        <a class="dropdown-item" href="#">Load</a>
+      </div>
+    </div>
+  </div>
+</nav>
 </template>
