33236225d6b43aeb2f46d387c8f49a71c56f58af
tealicious <carterdanielr@gmail.com>
2018-01-13 21:22:41 -0500

    .

---
+++ vue-cli/3-slots-and-advanced-component-usage/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <!-- slots can be given specificity -->
+          <h2 slot="title">{{quoteTitle}}</h2>
+          <!-- unassigned content gets placed into the unnamed slot -->
+          <p>{{quote}}</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote.vue";
+export default {
+  data() {
+    return {
+      quoteTitle: "The Quote",
+      quote: "A wonderful quote!"
+    };
+  },
+  components: {
+    appQuote: Quote
+  }
+};
+</script>
+
+<style scoped>
+/* scoped styles get passed down into child slots */
+h2 {
+  color: red;
+}
+</style>
---
+++ vue-cli/3-slots-and-advanced-component-usage/src/components/Quote.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <slot name="title"></slot>
+        <span style="color:#ccc;">
+            <!-- default content can be placed into a slot, and will render if nothing is passed into the slot  from the parent -->
+            <slot name="subtitle">The Subtitle</slot>
+        </span>
+        <slot>
+        </slot>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+div {
+  border: 2px solid lightgray;
+  text-align: center;
+  padding: 20px;
+  margin: 20px;
+}
+</style>
