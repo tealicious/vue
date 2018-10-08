5fef86e3081ac49a39707e2a3b3f0c9bb81b31c5
tealicious <carterdanielr@gmail.com>
2018-10-08 00:01:58 -0400

    .

--- stock-trader/src/store/actions.js
+++ stock-trader/src/store/actions.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { fireBase } from "../main";
 export const loadData = ({ commit }) => {
   axios.get(`${fireBase}data.json`).then(res => {
-    console.log(res.data.portfolio);
     commit("LOAD_PORTFOLIO", res.data.portfolio);
   });
 };
