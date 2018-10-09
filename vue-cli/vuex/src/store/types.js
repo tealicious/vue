8150829debb569a33c50dd9b215ab07403bbad36
tealicious <carterdanielr@gmail.com>
2018-10-09 13:28:23 -0400

    .

---
+++ vue-cli/vuex/src/store/types.js
@@ -0,0 +1,5 @@
+export const DOUBLE_COUNTER = 'counter/DOUBLE_COUNTER';
+export const CLICK_COUNTER = 'counter/CLICK_COUNTER';
+
+// NOW AUTMOMATED IN VUE2 > https://github.com/vuejs/vuex/releases/tag/v2.1.0
+// way to prevent accidentally assigning the same name to something. provide namespacing here (you will never do this) and call to it from another file
