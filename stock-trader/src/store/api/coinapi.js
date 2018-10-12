b57e87fc64bbb98d2f6e9b71f83529e14364a40d
tealicious <carterdanielr@gmail.com>
2018-10-12 17:29:58 -0400

    coinapi back to where it was at

--- stock-trader/src/store/api/coinapi.js
+++ stock-trader/src/store/api/coinapi.js
@@ -34,13 +34,15 @@ export default class CoinApi extends Api {
     for (let coin of coinArray) {
       resolvedCoins.promises.push(
         new Promise((resolve, reject) => {
-          this.get({ resource: `/price?fsym=${coin.Symbol}&tsyms=USD` }).then(
-            function(response) {
+          this.get({ resource: `/price?fsym=${coin.Symbol}&tsyms=USD` })
+            .then(response => {
               coin.Price = response.data.USD;
               resolvedCoins.coins.push(coin);
               resolve();
-            }
-          );
+            })
+            .catch(() => {
+              reject();
+            });
         })
       );
     }
