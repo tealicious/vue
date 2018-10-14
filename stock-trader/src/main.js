c49c3f64e719ca9130db772877a32314a8165655
tealicious <carterdanielr@gmail.com>
2018-10-13 20:46:05 -0400

    adding a chart lib

--- stock-trader/src/main.js
+++ stock-trader/src/main.js
@@ -5,9 +5,12 @@ import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
+import VueApexCharts from "vue-apexcharts";
 import "./scss/app.scss";
 export const fireBase = "https://stock-trader-b44bf.firebaseio.com/";
 export const cryptoCompare = "https://min-api.cryptocompare.com/data";
+
+Vue.use(VueApexCharts);
 
 Vue.config.productionTip = false;
 
--- stock-trader/src/views/Home.vue
+++ stock-trader/src/views/Home.vue
@@ -1,19 +1,51 @@
 <template>
   <div class="stocks">
     <h1>{{ name }}</h1>
-    <h2 class="mt-3"><strong>Funds: {{funds | currency}}</strong></h2>
-    <h2 class="mt-3"><strong>Portfolio: {{portfolioValue | currency}}</strong></h2>
-    <h2 class="mt-3"><strong>Total: {{totalWorth | currency}}</strong></h2>
-    <h2 class="mt-3"><strong>Diversity: {{coinCount}} Coins</strong></h2>
+    <h2 class="mt-3">
+      <strong>Funds: {{funds | currency}}</strong>
+    </h2>
+    <h2 class="mt-3">
+      <strong>Portfolio: {{portfolioValue | currency}}</strong>
+    </h2>
+    <h2 class="mt-3">
+      <strong>Total: {{totalWorth | currency}}</strong>
+    </h2>
+    <h2 class="mt-3">
+      <strong>Diversity: {{coinCount}} Coins</strong>
+    </h2>
+    <apexcharts type="donut" :options="chartOptions" :series="series">
+    </apexcharts>
   </div>
 </template>
 
 <script>
+import VueApexCharts from "vue-apexcharts";
 export default {
+  components: {
+    apexcharts: VueApexCharts
+  },
   data() {
     return {
       name: "Home"
     };
+  },
+  computed: {
+    chartOptions() {
+      const coinNames = [];
+      for (let coin of this.$store.getters.portfolio) {
+        coinNames.push(coin.CoinName);
+      }
+      return {
+        labels: coinNames
+      };
+    },
+    series() {
+      const coinCount = [];
+      for (let coin of this.$store.getters.portfolio) {
+        coinCount.push(coin.Quantity * coin.Price);
+      }
+      return coinCount;
+    }
   }
 };
 </script>
