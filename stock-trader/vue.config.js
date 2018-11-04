a58d1845e8fd64682aad7bc7db6ac96d28270a5b
tealicious <carterdanielr@gmail.com>
2018-11-04 03:37:27 -0500

    deploy script

--- stock-trader/vue.config.js
+++ stock-trader/vue.config.js
@@ -7,5 +7,5 @@ module.exports = {
       }
     }
   },
-  baseUrl: process.env.NODE_ENV === "production" ? "/production-sub-path/" : "/"
+  baseUrl: process.env.NODE_ENV === "production" ? "/cointrader/" : "/"
 };
